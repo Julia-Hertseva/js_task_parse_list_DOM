4e078082ef57555cba6fcc47fe860992f9e6d33d
Julia-Hertseva <gerceva@gmail.com>
2025-09-26 21:14:29 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,46 @@
 'use strict';
 
-// write code here
+function sortBySalary() {
+  const employeeList = document.querySelectorAll('li');
+  const employeesArray = Array.from(employeeList);
+
+  employeesArray.sort((a, b) => {
+    const salaryA = parseFloat(
+      a.getAttribute('data-salary').replace('$', '').replace(',', ''),
+    );
+    const salaryB = parseFloat(
+      b.getAttribute('data-salary').replace('$', '').replace(',', ''),
+    );
+
+    return salaryB - salaryA;
+  });
+
+  const ul = document.querySelector('ul');
+
+  employeesArray.forEach((employee) => ul.appendChild(employee));
+}
+
+function getEmployeesArray() {
+  const employeeList = document.querySelectorAll('li');
+  const employees = [];
+
+  employeeList.forEach((item) => {
+    const employee = {
+      name: item.textContent.trim(),
+      position: item.getAttribute('data-position'),
+      salary: parseFloat(
+        item.getAttribute('data-salary').replace('$', '').replace(',', ''),
+      ),
+      age: Number(item.getAttribute('data-age')),
+    };
+
+    employees.push(employee);
+  });
+
+  return employees;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  sortBySalary();
+  getEmployeesArray();
+});
